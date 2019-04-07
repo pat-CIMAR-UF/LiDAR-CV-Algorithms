c9e0f61fa78235cf164edf1b1a39b118c077076a
Yiqun-Pat-Wang <36198135+pat-CIMAR-UF@users.noreply.github.com>
2019-04-07 11:55:20 -0400

    Update video.py

--- video.py
+++ video.py
@@ -1,20 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Mar 13 15:48:47 2019
+Created on Thu Mar 14 23:13:07 2019
 
-@author: yq
+@author: Yiqun
 """
 
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
+import time
 
 def make_coordinates(image, line_parameters):
     slope, intercept = line_parameters
+    print('slope type:')
+    print(type(slope))
+    print('intercept type:')
+    print(type(intercept))
     y1 = image.shape[0]
     y2 = int(y1*(3/5))
     x1 = int((y1 - intercept)/slope)
-    x2 = int((y2 - intercept)/slope)
+    x2 = int((y2 - intercept)/slope) 
+    if x1 > 2000 or x2 > 2000:
+        x1 = 1894
+        x2 = 1323
+    print(np.array([x1, y1, x2, y2]))
     return np.array([x1, y1, x2, y2])
 
 def average_slope_intercept(image,lines):
@@ -25,12 +33,21 @@ def average_slope_intercept(image,lines):
         parameters = np.polyfit((x1,x2), (y1, y2), 1)
         slope = parameters[0]
         intercept = parameters[1]
+        print("slope: %f, inter: %f" % (slope, intercept))
         if slope < 0:
             left_fit.append((slope, intercept))
         else:
             right_fit.append((slope, intercept))
+    if len(left_fit) == 0:
+        left_fit.append((-9.85185185*0.1, 1.04825185*1000))
+    if len(right_fit) == 0:
+        right_fit.append((1.39272446,  -523.417127))  
     left_fit_average = np.average(left_fit, axis = 0)
+    print("left_fit")
+    print(left_fit_average)
     right_fit_average = np.average(right_fit, axis = 0)
+    print("right_fit")
+    print(right_fit_average)
     left_line = make_coordinates(image, left_fit_average)
     right_line = make_coordinates(image, right_fit_average)
     return np.array([left_line, right_line])
@@ -46,22 +63,25 @@ def canny(image):
     # Canny edge detection,which takes gradient
     # 50 and 150 are the low threshold and high threshold
     # find the sharpest change in intensity
-    canny = cv2.Canny(blur, 50, 150)
+    canny = cv2.Canny(blur, 30, 150)
     return canny
     
     
 def display_lines(image, lines):
     line_image = np.zeros_like(image)
     if lines is not None:
-        for line in lines:
-            x1,y1,x2,y2 = line.reshape(4)
-            cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 10)
+        for x1,y1,x2,y2 in lines:
+            print("x1: %d, y1: %d, x2: %d, y2: %d" %(x1,y1,x2,y2))
+            if x1 < 0 or x2 < 0:
+                x1 = 85
+                x2 = 477
+            cv2.line(line_image, (x1,y1), (x2,y2), (255, 0, 0), 6)
     return line_image
     
 def region_of_interest(image):
-    height = image.shape[0]
+    # height = image.shape[0]
     polygons = np.array([
-    [(200, height), (1100, height), (550, 250)]
+    [(550, 435),(220, 740),(1075, 740), (730,435)]
     ])
     
     mask = np.zeros_like(image)
@@ -82,19 +102,34 @@ def region_of_interest(image):
 #cv2.waitKey(0)
 
 
-cap = cv2.VideoCapture("test2.mp4")
+cap = cv2.VideoCapture("record1.mp4")
 while(cap.isOpened()):
+    print('******************************************')
     _, frame = cap.read()
     canny_image = canny(frame)
     cropped_image = region_of_interest(canny_image)
-    lines = cv2.HoughLinesP(cropped_image, 2, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap = 5)
-    averaged_lines = average_slope_intercept(frame, lines)
-    line_image = display_lines(frame, averaged_lines)
-    combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    cv2.imshow("result", combo_image)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+#    cv2.imshow("result", canny_image)
+#    if cv2.waitKey(1) & 0xFF == ord('q'):
+#            break
+    lines = cv2.HoughLinesP(cropped_image, 3, np.pi/180, 100, np.array([]), minLineLength=40, maxLineGap = 5)
+    print("lines:")
+    print(lines)
+    if lines is not None:
+        averaged_lines = average_slope_intercept(frame, lines)
+        print("averaged_lines:")
+        print(averaged_lines)
+        line_image = display_lines(frame, averaged_lines)
+        combo_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
+        print('finish')
+        cv2.imshow("result", combo_image)
+        time.sleep(0.1)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        cv2.imshow("result", frame)
+        time.sleep(0.1)
     
+
 cap.release()
-cv2.detroyAllWindows()
+cv2.destroyAllWindows()
     
