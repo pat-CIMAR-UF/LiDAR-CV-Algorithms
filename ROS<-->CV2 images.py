9aef33cc1869745b9ab5263ded8849b4cb979fe9
Yiqun-Pat-Wang <36198135+pat-CIMAR-UF@users.noreply.github.com>
2019-04-15 18:58:02 -0400

    Update ROS<-->CV2 images.py

--- ROS<-->CV2 images.py
+++ ROS<-->CV2 images.py
@@ -11,40 +11,32 @@ from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
 class image_converter:
-
-  def __init__(self):
-    self.image_pub = rospy.Publisher("/camera/image_edit",Image, queue_size=10)
-
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/camera/image_color",Image,self.callback) #2.Subscribe the topic from ROS bag file, start the callback
-
-  def callback(self,data):
+    def __init__(self):
+        self.image_pub = rospy.Publisher("/camera/image_edit", Image, queue_size = 10)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/camera/image_color", Image, self.callback)
+    
+    def callback(self,data):
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+    
     try:
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8") #convert the ROS image msg to cv2 format image message, as "bgr8" format
+        self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image,"bgr8"))
     except CvBridgeError as e:
-      print(e)
+        print(e)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
-
-    #cv2.imshow("Image window", cv_image)
-    #cv2.waitKey(3)
-
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin() # you start the program here
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    ic = image_converter()
+    rospy.init_node('image_converter', anonymous = True)
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
-#sense
+
