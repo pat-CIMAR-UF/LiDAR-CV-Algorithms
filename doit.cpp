4d86506eeefc1204868838d883df584a16ef2a09
Yiqun-Pat-Wang <36198135+pat-CIMAR-UF@users.noreply.github.com>
2019-06-06 10:58:43 -0400

    Add files via upload

--- doit.cpp
+++ doit.cpp
@@ -1,36 +1,40 @@
 #include <ros/ros.h>
-// PCL specific includes
-#include <sensor_msgs/PointCloud2.h>
-#include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
-#include <pcl/point_types.h>
-#include <pcl_ros/point_cloud.h>
-
-#include <boost/foreach.hpp>
-
-void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& msg){
-    
-    printf("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-    
-    // Container for data
-    pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-    pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-
-    // Convert to PCL data type
-    pcl_conversions::toPCL(*msg, *cloud);
-
-    BOOST_FOREACH(const pcl::PointXYZ& pt, cloud->points)
-        printf("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
-}
-
-int main(int argc, char** argv){
-    // Initialize ROS
-    ros::init(argc, argv, "doit");
+#include <pcl_conversions/pcl_conversions.h>
+#include <sensor_msgs/PointCloud2.h>
+ 
+main (int argc, char **argv)
+{
+    ros::init (argc, argv, "pcl_create");
+ 
     ros::NodeHandle nh;
-
-    ros::Subscriber sub = nh.subscribe<sensor_msgs::PointCloud2> ("/velodyne_points", 100, cloud_cb);
-
-    // pub = nh.advertise<sensor_msgs::PointCloud2> ("/output", 1);
-
-    ros::spin();
+    ros::Publisher pcl_pub = nh.advertise<sensor_msgs::PointCloud2> ("pcl_output", 1);
+    pcl::PointCloud<pcl::PointXYZ> cloud;
+    sensor_msgs::PointCloud2 output;
+ 
+    // Fill in the cloud data
+    cloud.width  = 100;
+    cloud.height = 1;
+    cloud.points.resize(cloud.width * cloud.height);
+ 
+    for (size_t i = 0; i < cloud.points.size (); ++i)
+    {
+        cloud.points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
+        cloud.points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
+        cloud.points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+    }
+ 
+    //Convert the cloud to ROS message
+    pcl::toROSMsg(cloud, output);
+    output.header.frame_id = "my_frame";
+ 
+    ros::Rate loop_rate(1);
+    while (ros::ok())
+    {
+        pcl_pub.publish(output);
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
+ 
+    return 0;
 }
--- subscribe_pc.cpp
+++ subscribe_pc.cpp
@@ -3,19 +3,24 @@
 #include <pcl/point_types.h>
 #include <boost/foreach.hpp>
 
-typedef pcl::PointCloud<pcl::PointXYZ> PointCloud;
+typedef pcl::PointCloud<pcl::PointXYZI> PointCloud;
 
 void callback(const PointCloud::ConstPtr& msg)
 {
   printf ("Cloud: width = %d, height = %d\n", msg->width, msg->height);
-  BOOST_FOREACH (const pcl::PointXYZ& pt, msg->points)
-    printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+  //BOOST_FOREACH (const pcl::PointXYZI& pt, msg->points)
+    //printf ("\t(%f, %f, %f)\n", pt.x, pt.y, pt.z);
+
+  BOOST_FOREACH (const pcl::PointXYZI& pt, msg->points){
+    printf ("\t(%f, %f, %f, %f)\n", pt.x, pt.y, pt.z, pt.intensity);
+  }
+  
 }
 
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "sub_pcl");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe<PointCloud>("points2", 1, callback);
+  ros::Subscriber sub = nh.subscribe<PointCloud>("/velodyne_points", 1, callback);
   ros::spin();
 }
